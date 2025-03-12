d52aa365f6c161e49c652c826a7047517f448f42
Ben Pearson <benpearson61@googlemail.com>
2025-03-12 14:35:14 +0900

    added switchable containers

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-input = document.getElementById("selection")
-console.log(input)
+const input = document.getElementById("selection")
+const contentBox = document.getElementById("content")
+const bio = document.getElementById("bio").innerHTML
+const stack = document.getElementById("stack").innerHTML
+const projects = document.getElementById("projects").innerHTML
+const contact = document.getElementById("contact").innerHTML
 
-charArray = []
-selectionText = []
+
+let charArray = []
+let selectionText = []
 
 document.addEventListener("keydown", (event) => {
-  console.log(event)
   key = event.keyCode
   if (key >= 65 && key <= 90 || (key >= 48 && key <= 57) || (key >= 97 && key <= 122)) {
     selectionText.push(event.key)
@@ -14,14 +18,31 @@ document.addEventListener("keydown", (event) => {
     selectionText.pop()
     input.innerText = selectionText.join("")
   } else if (key == 13) {
-    console.log(selectionText.join().toUpperCase())
     switch (selectionText.join("").toUpperCase()) {
+      case "BIO":
+        input.innerText = "";
+        selectionText = [];
+        contentBox.innerHTML = bio;
+        console.log(bio)
+        break;
       case "STACK":
-        input.innerText += "\n RUBY!"
+        input.innerText = ""
+        selectionText = []
+        contentBox.innerHTML = stack;
+        break;
       case "PROJECTS":
-
+        input.innerText = ""
+        selectionText = []
+        contentBox.innerHTML = projects;
+        break;
       case "CONTACT":
-
+        input.innerText = ""
+        selectionText = []
+        contentBox.innerHTML = contact;
+        break;
+      default:
+        input.innerText += "\nCommand Not Recognised\n"
+        break;
     }
 
   }
